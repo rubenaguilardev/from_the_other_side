df5eaa9b32a9bfdb1c713f02b5c79cbf07ed4185
Ruben Aguilar <ragui56@wgu.edu>
2026-03-10 23:11:56 -0700

    add basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+import http from 'node:http'
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.statusCode = 200
+  res.end('<html><h1>The server is working</h1></html>')
+})
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
