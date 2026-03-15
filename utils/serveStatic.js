103a3c03d836bca3d28baeb0c1c8648eab7bee48
Ruben Aguilar <ragui56@wgu.edu>
2026-03-14 18:01:50 -0700

    serve 404 page, server err

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -14,7 +14,12 @@ const serveStatic = async (req, res, dir) => {
     const content = await fs.readFile(filePath)
     sendResponse(res, 200, getContentType(ext), content)
   }catch(err) {
-    console.log(err)
+    if (err.code === 'ENOENT') {
+      const content = await fs.readFile(path.join(publicDir, '404.html'))
+      sendResponse(res, 404, 'text/html', content)
+    } else {
+      sendResponse(res, 500, 'text/html', '<html><h1>Server Error: ${err.code}</h1></html>')
+    }
   }
 
 }
