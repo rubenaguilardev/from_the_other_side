24192c7cd419e2d950db140a5b8dbd44da3d9899
Ruben Aguilar <ragui56@wgu.edu>
2026-03-14 17:41:00 -0700

    serve the frontend

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,14 +1,18 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
 
 const serveStatic = async (req, res, dir) => {
 
-  const filePath = path.join(dir, 'public', 'index.html')
+  const publicDir = path.join(dir, 'public')
+  const filePath = path.join(publicDir, req.url === '/' ? 'index.html' : req.url)
+
+  const ext = path.extname(filePath)
 
   try {
     const content = await fs.readFile(filePath)
-    sendResponse(res, 200, 'text/html', content)
+    sendResponse(res, 200, getContentType(ext), content)
   }catch(err) {
     console.log(err)
   }
